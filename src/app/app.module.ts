ed75133d3234ae6870245ab150d9e44605fe3d11
unknown <mjpannu1@gmail.com>
2019-02-21 23:15:06 +0530

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+
 import { ErrorHandler, NgModule } from '@angular/core';
+
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MusicPlayerPage } from '../pages/music-player/music-player';
 import { ListPage } from '../pages/list/list';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { MusicsProvider } from '../providers/musics/musics';
+import { Media } from '@ionic-native/media/ngx';
+
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    MusicPlayerPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    MusicPlayerPage
   ],
   providers: [
+    SocialSharing,
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MusicsProvider,
+    MusicsProvider,
+    Media
+    
+    
   ]
 })
 export class AppModule {}
